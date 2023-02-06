ef55ec99309710e6a5008e2992ab3c4cbcc67c6d
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2023-02-06 00:53:20 -0500

    db instance no longer an export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ require("dotenv").config();
 import { ExtendedClient } from "./classes/Client";
 import { DB } from "./classes/Mongo"
 export const client = new ExtendedClient();
-export const db = new DB();
+const db = new DB();
 client.start();
 db.connect();
