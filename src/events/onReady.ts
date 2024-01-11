3b94a543d5fd3b7a9840f2df16c9dfc201dbf1b6
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2024-01-11 17:55:23 -0500

    Update onReady.ts

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -4,4 +4,19 @@ import { client } from ".."
 export default new Event("ready", () => {
     console.log(`${client?.user.tag} is now online!`);
     client.user.setActivity()
+    async function checkMissedMembers(id = process.env.joinId_1) {
+        var guild = client.guilds.cache.get(id)
+        if (!guild) return console.log(`Unable to find guild by id: ${id}`)
+        let minimumRoles = ['872314071347773451', '863935649240842271', '863935648951304194']
+        guild.members.cache.each(member => {
+            if (member.user.bot === true || member.user.id === "<YourIdHere>") return;
+            let userRoles = []
+            member.roles.cache.each(role => userRoles.push(role.id))
+            minimumRoles.forEach(async r => {
+                if (userRoles.indexOf(r) === -1) return member.roles.add(r).catch((e: any) => console.log(e))
+            })
+        })
+        console.log("[Role Manager] ><> All members have been updated! ><>")
+    }
+    checkMissedMembers()
 });
