43d579dd870385072e7d9fb47c5cfc4152c18139
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2024-01-12 12:17:05 -0500

    Update memberLeave.ts

--- src/events/memberLeave.ts
+++ src/events/memberLeave.ts
@@ -4,11 +4,14 @@ import { Event } from "../classes/Event";
 export default new Event("guildMemberRemove", member => {
     const guild = member.guild.id
     const cache = member.guild.channels.cache;
+    let minimumRoles = ['', '', '']
+    member.roles.cache.each(role => minimumRoles.indexOf(role.id) === -1  ? userRoles.push(role.id+" : "+role.name) : null)
+    let summary = `USER: **${member.user.username}** \nROLES:\n${userRoles.join("\n")} \nJOINED: *${member.joinedAt}* `
     switch(guild) {
         case process.env.joinId_1:
             const mods = cache.find((ch:any) => ch.id === process.env.modChannelId);    
             if (!mods) return;
-            (mods as TextChannel).send(`${member}:${member.id} has left the server . . .`)
+            (mods as TextChannel).send(`**[Event: memberLeave]**\n${summary}`)
             .catch((leaveError: any) => console.log(`[ MemberLeave Error ] ${leaveError}`));
         break;
     }
